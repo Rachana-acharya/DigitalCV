bffa78797d688f7fba807d843732e2223e70d22d
Rachana-acharya <108857073+Rachana-acharya@users.noreply.github.com>
2022-08-05 19:03:36 +0530

    rachana

---
+++ project.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+//import { Angular } from '../angular';
+//import { AngularServiceService } from '../angular-service.service';
+//import { AngularServiceService }
+@Component({
+  selector: 'app-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent implements OnInit {
+  //angularcourse:Angular[]=[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    //const angularObservable=this.angularservice.getdetails();
+    //angularObservable.subscribe((details:Angular[])=>{
+      //this.angularcourse=details;
+  }//);
+
+}
